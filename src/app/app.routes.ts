d6dcdc599d59515a7ea1e9b2ba2eb87ac3d88064
jesus- <guerrero.jesussdavid@gmail.com>
2024-04-21 12:52:03 -0500

    Chucho: portafolio01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/homePage/homePage.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
+    { path: 'portafolio', component: PortafolioComponent},
 
 ];
